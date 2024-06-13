50d026a4a81d2136b60adad403166d08286f1bc2
Ananthan493 <ananthanofficial493@gmail.com>
2024-06-13 11:24:55 +0530

    signup done

---
+++ models/blog.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema(
+    {
+        "name":{type:String,required:true},
+        "email":{type:String,required:true},
+        "password":{type:String,required:true}
+    }
+)
+
+let blogmodel = mongoose.model("users",Schema)
+module.exports = {blogmodel}
